bd51dd32f4321974e9d43ded50dce1464d727941
devanshi22mehta <devanshimehta73@gmail.com>
2025-03-11 15:59:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function playAudio(id) {
-    let audio = document.getElementById(id);
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
-    }
+function playAudio(id,button) {
+  let audio = document.getElementById(id);
+if (audio.paused) {
+  audio.play();
+  button.textContent = "⏸";
+} else {
+  audio.pause();
+  button.textContent = "▶";
+}
 }
 
-
+// function playAudio(id) {
+//   let audio = document.getElementById(id);
+//   if (audio.paused) {
+//       audio.play();
+//   } else {
+//       audio.pause();
+//   }
+// }    
 
 const breathText = document.getElementById("breathText");
 
@@ -38,44 +47,8 @@ function previewImage(event) {
     }
 }
 
-// upload documents
-window.addEventListener("load",()=>{
-    const input = document.getElementById("upload");
-    const filewrapper=document.getElementById("filewrapper");
 
-    input.addEventListener("change",(e)=>{
-        let fileName = e.target.files[0].name;
-        let filetype = e.target.value.split(".").pop();
-        fileshow(fileName, filetype);
-    })
- 
-    const fileshow=(fileName,filetype)=>{
-        const showfileboxElem = document.createElement("div");
-        showfileboxElem.classList.add("showfilebox");
-        const leftElem = document.createElement("div");
-        leftElem.classList.add("left")
-        const fileTypeElem = document.createElement("span");
-        fileTypeElem.classList.add("filetype");
-        fileTypeElem.innerHTML=filetype;
-        leftElem.append(fileTypeElem);
-        const filetitleElem = document.createElement("h3");
-        filetitleElem.innerHTML=fileName;
-        leftElem.append(filetitleElem);
-        showfileboxElem.append(leftElem);
-        const rightElem = document.createElement("div");
-        rightElem.classList.add("right");
-        showfileboxElem.append(rightElem);
-        const crossElem = document.createElement("span");
-        crossElem.innerHTML="&#215;";
-        rightElem.append(crossElem);
-        filewrapper.append(showfileboxElem); 
-
-        crossElem.addEventListener("click",()=>{
-            filewrapper.removeChild(showfileboxElem);
-        })
-    }
-})
-
+// upload document ki style is in the html file itself
 
 
 
